537f2f6b586539472ebd7eff1a89387a5d6ab4f1
dongsu_ko <127917642+kdongsu5509@users.noreply.github.com>
2025-05-10 19:24:42 +0900

    refactor: delte unnecessary files

--- android/app/src/main/kotlin/com/example/knucl_face_analyzer_2025/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.knucl_face_analyzer_2025
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/DataLayer/Repository/ImageRepository.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:typed_data';
-
-abstract class ImageRepository {
-  Future<String> saveImage(Uint8List encodedImage);
-  String getImage(int id);
-  Future<bool> deleteImage(int id);
-}
