086c73b6faa972f944c44b01b1b147bdb5e3f874
dongsu_ko <127917642+kdongsu5509@users.noreply.github.com>
2025-05-11 23:59:00 +0900

    feat(#issue4) : add dark and light mode (#5)

---
+++ Mobile/lib/DataLayer/Service/storage/prefs.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+part 'prefs.g.dart';
+
+@riverpod
+Future<SharedPreferencesWithCache> prefs(Ref ref) =>
+    SharedPreferencesWithCache.create(
+      cacheOptions: const SharedPreferencesWithCacheOptions(),
+    );
--- Mobile/lib/View/home_view.dart
+++ Mobile/lib/View/home_view.dart
@@ -1,74 +1,93 @@
+import 'package:ai_fortune_teller_app/common/default_view.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../common/device_info.dart';
+import '../setting/app_theme_provider.dart';
 import 'info_input_view.dart';
 
-class HomeView extends StatefulWidget {
+class HomeView extends ConsumerStatefulWidget {
     const HomeView({super.key});
 
     @override
-    State<HomeView> createState() => _HomeViewState();
+    ConsumerState<HomeView> createState() => _HomeViewState();
 }
 
-class _HomeViewState extends State<HomeView> {
+class _HomeViewState extends ConsumerState<HomeView> {
     @override
     Widget build(BuildContext context) {
         String title1 = "ChatGPT가 분석해주는";
         String title2 = "나의 사주";
 
-        return Scaffold(
-            body: GestureDetector(
-                onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => InfoInputView()));
-                },
-                child: Container(
-                    decoration: BoxDecoration(
-                        color: Colors.white
-                    ),
-                    width: double.infinity, // 전체 화면 너비
-                    height: double.infinity, // 전체 화면 높이
-                    child: Stack(
-                        alignment: Alignment.bottomCenter,
-                        children: [
-                            Center(
-                                child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                        applicationTitle(context, title1, title2)
-                                    ]
-                                )
-                            ),
-                            Padding(
-                                padding: EdgeInsets.symmetric(
-                                    vertical: MediaQuery.of(context).size.height * 0.1
+        return DefaultView(child: Scaffold(
+                body: GestureDetector(
+                    onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => InfoInputView()));
+                    },
+                    child: Container(
+                        decoration: BoxDecoration(
+                        ),
+                        width: double.infinity, // 전체 화면 너비
+                        height: double.infinity, // 전체 화면 높이
+                        child: Stack(
+                            alignment: Alignment.bottomCenter,
+                            children: [
+                                Center(
+                                    child: Column(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        children: [
+                                            ApplicationTitle(title1: title1, title2: title2)
+                                        ]
+                                    )
                                 ),
-                                child: Text("  아무 곳이나\n터치 하여 시작", style: TextStyle(fontSize: DeviceInfo.deviceInfo.contains("Windows") ? MediaQuery.of(context).size.width * 0.03 : MediaQuery.of(context).size.width * 0.05, color: Colors.grey)))
-                        ]
+                                Padding(
+                                    padding: EdgeInsets.symmetric(
+                                        vertical: MediaQuery.of(context).size.height * 0.1
+                                    )
+                                )
+                            ]
+                        )
                     )
                 )
             )
         );
     }
 }
+class ApplicationTitle extends ConsumerWidget {
+    final String title1;
+    final String title2;
 
-Widget applicationTitle(BuildContext context, String title1, String title2) {
-    return Column(
-        children: [
-            Text(
-                title1,
-                style: titleStyle(context, 0.07),
-                textAlign: TextAlign.center
-            ),
-            Padding(padding: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.02)),
-            Text(
-                title2,
-                style: titleStyle(context, 0.09),
-                textAlign: TextAlign.center
-            )
-        ]
-    );
+    const ApplicationTitle({required this.title1, required this.title2, super.key});
+
+    @override
+    Widget build(BuildContext context, WidgetRef ref) {
+        return Column(
+            children: [
+                IconButton(
+                    icon: const Icon(Icons.brightness_6),
+                    onPressed: () {
+                        final notifier = ref.read(currentThemeModeProvider.notifier);
+                        notifier.state = notifier.state == ThemeMode.dark
+                            ? ThemeMode.light
+                            : ThemeMode.dark;
+                    }
+                ),
+                Text(
+                    title1,
+                    style: titleStyle(context, 0.07),
+                    textAlign: TextAlign.center
+                ),
+                Padding(padding: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.02)),
+                Text(
+                    title2,
+                    style: titleStyle(context, 0.09),
+                    textAlign: TextAlign.center
+                )
+            ]
+        );
+    }
 }
+
 
 TextStyle titleStyle(BuildContext context, double fontSize) {
     return TextStyle(
