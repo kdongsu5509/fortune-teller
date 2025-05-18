3dc743367abeab2649e2fff71383eb59188701e2
dongsu_ko <127917642+kdongsu5509@users.noreply.github.com>
2025-05-18 11:39:24 +0900

    refact/ add swagger comment

--- BackEnd/src/main/java/com/jangpyeong/fortuneteller/support/response/APIResponse.java
+++ BackEnd/src/main/java/com/jangpyeong/fortuneteller/support/response/APIResponse.java
@@ -2,15 +2,32 @@ package com.jangpyeong.fortuneteller.support.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
+@Schema(
+        description = "공통 APIResponse",
+        title = "APIResponse",
+        example = """
+                {
+                    "code": 200,
+                    "message": "OK",
+                    "data": {
+                        // Your data here
+                    }
+                }
+                """
+)
 public class APIResponse<T> {
+    @Schema(description = "HTTP 상태 코드", example = "200")
     private final int code;
+    @Schema(description = "응답 메시지", example = "OK")
     private final String message;
 
     @JsonInclude(NON_NULL)
+    @Schema(description = "응답 데이터")
     private final T data;
 
     private APIResponse(int code, String message, T data) {
