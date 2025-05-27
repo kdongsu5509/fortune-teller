b133775e1b7dea523569b01addef48163150378c
dongsu_ko <127917642+kdongsu5509@users.noreply.github.com>
2025-05-28 00:20:37 +0900

    Dongsu back (#34)
    * refact/ .
    
    * feat/ add imgage upload contorller
    
    * fix/ fix typo
    
    * feat/ return iamge addr

---
+++ BackEnd/src/main/java/com/jangpyeong/fortuneteller/domain/image/api/ImageController.java
@@ -0,0 +1,34 @@
+package com.jangpyeong.fortuneteller.domain.image.api;
+
+import com.jangpyeong.fortuneteller.domain.analyze.application.ImageService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/image")
+@Tag(
+        name = "Image",
+        description = "이미지 업로드 API"
+)
+public class ImageController {
+
+    ImageService imageService;
+
+    @PostMapping("/upload")
+    public String uploadImage(MultipartFile file) {
+        String uploadedAddress;
+        try {
+            uploadedAddress = imageService.uploadImgae(file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return uploadedAddress;
+    }
+}
