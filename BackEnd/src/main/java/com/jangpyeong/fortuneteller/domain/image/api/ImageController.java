d033eca886f8809c7cf0f754008a8fe5d2844134
dongsu_ko <127917642+kdongsu5509@users.noreply.github.com>
2025-05-28 00:18:23 +0900

    feat/ return iamge addr

--- BackEnd/src/main/java/com/jangpyeong/fortuneteller/domain/image/api/ImageController.java
+++ BackEnd/src/main/java/com/jangpyeong/fortuneteller/domain/image/api/ImageController.java
@@ -1,6 +1,7 @@
-package com.jangpyeong.fortuneteller.domain.image;
+package com.jangpyeong.fortuneteller.domain.image.api;
 
 import com.jangpyeong.fortuneteller.domain.analyze.application.ImageService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,16 +12,23 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/image")
+@Tag(
+        name = "Image",
+        description = "이미지 업로드 API"
+)
 public class ImageController {
 
     ImageService imageService;
 
     @PostMapping("/upload")
-    public void uploadImage(MultipartFile file) {
+    public String uploadImage(MultipartFile file) {
+        String uploadedAddress;
         try {
-            imageService.uploadImgae(file);
+            uploadedAddress = imageService.uploadImgae(file);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+        return uploadedAddress;
     }
 }
