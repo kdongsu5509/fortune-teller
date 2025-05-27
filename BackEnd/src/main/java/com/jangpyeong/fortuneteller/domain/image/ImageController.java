3493b1297bcfe157735a332fb2fdc77c29fd0bd9
dongsu_ko <127917642+kdongsu5509@users.noreply.github.com>
2025-05-28 00:15:48 +0900

    fix/ fix typo

--- BackEnd/src/main/java/com/jangpyeong/fortuneteller/domain/image/ImageController.java
+++ BackEnd/src/main/java/com/jangpyeong/fortuneteller/domain/image/ImageController.java
@@ -4,11 +4,13 @@ import com.jangpyeong.fortuneteller.domain.analyze.application.ImageService;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@RestController
 @RequiredArgsConstructor
-@RestController("/image")
+@RequestMapping("/image")
 public class ImageController {
 
     ImageService imageService;
