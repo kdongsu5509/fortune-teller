f877a69ce7cd3840a8a365881e902e97c437fa43
dongsu_ko <127917642+kdongsu5509@users.noreply.github.com>
2025-05-28 23:07:28 +0900

    refact/ 주석 삭제

--- BackEnd/src/main/java/com/jangpyeong/fortuneteller/common/config/RedisConfig.java
+++ BackEnd/src/main/java/com/jangpyeong/fortuneteller/common/config/RedisConfig.java
@@ -6,17 +6,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-/**
- * Redis 설정 클래스
- * <p>
- * RedisTemplate을 사용하여 Redis에 객체를 저장하고 조회할 수 있도록 설정합니다.
- */
-
 @Configuration
+@EnableRedisRepositories
 public class RedisConfig {
+
     @Bean
     public RedisTemplate<String, JwtAuthRedis> jwtRedisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, JwtAuthRedis> template = new RedisTemplate<>();
